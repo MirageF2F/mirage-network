5e5cbe3056307158c25e0b31496b3647ec6d633a
Mirage <cognitiveorganism@gmail.com>
2025-11-11 17:41:08 +0100

    Create types.go

---
+++ pkg/adapters/http/client/types.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"context"
+
+	"github.com/miragef2f/go-peer/pkg/message/layer1"
+)
+
+type IClient interface {
+	GetIndex(context.Context) (string, error)
+
+	GetOnlines(context.Context) ([]string, error)
+	DelOnline(context.Context, string) error
+
+	GetConnections(context.Context) ([]string, error)
+	AddConnection(context.Context, string) error
+	DelConnection(context.Context, string) error
+
+	ProduceMessage(context.Context, layer1.IMessage) error
+}
+
+type IRequester interface {
+	GetIndex(context.Context) (string, error)
+
+	GetOnlines(context.Context) ([]string, error)
+	DelOnline(context.Context, string) error
+
+	GetConnections(context.Context) ([]string, error)
+	AddConnection(context.Context, string) error
+	DelConnection(context.Context, string) error
+
+	ProduceMessage(context.Context, layer1.IMessage) error
+}
