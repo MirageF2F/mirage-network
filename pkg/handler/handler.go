903b6a63c0ab7515583d680fed40c5d1985d9a86
Mirage <cognitiveorganism@gmail.com>
2025-11-11 17:48:35 +0100

    Create handler.go

---
+++ pkg/handler/handler.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"context"
+	"errors"
+
+	"github.com/miragef2f/go-peer/pkg/anonymity"
+	anon_logger "github.com/miragef2f/go-peer/pkg/anonymity/logger"
+	"github.com/miragef2f/go-peer/pkg/crypto/asymmetric"
+	internal_anon_logger "github.com/miragef2f/hidden-lake/internal/utils/logger/anon"
+	"github.com/miragef2f/hidden-lake/pkg/request"
+)
+
+func RequestHandler(pHandleF IHandlerF) anonymity.IHandlerF {
+	return func(
+		pCtx context.Context,
+		pNode anonymity.INode,
+		pSender asymmetric.IPubKey,
+		pReqBytes []byte,
+	) ([]byte, error) {
+		logger := pNode.GetLogger()
+		logBuilder := anon_logger.NewLogBuilder(pNode.GetSettings().GetServiceName())
+
+		// enrich logger
+		logBuilder.
+			WithSize(len(pReqBytes)).
+			WithPubKey(pSender)
+
+		// load request from message's body
+		loadReq, err := request.LoadRequest(pReqBytes)
+		if err != nil {
+			logger.PushErro(logBuilder.WithType(internal_anon_logger.CLogErroLoadRequestType))
+			return nil, errors.Join(ErrLoadRequest, err)
+		}
+
+		// handle request
+		switch rsp, err := pHandleF(pCtx, pSender, loadReq); {
+		case err != nil:
+			// internal logger
+			return nil, ErrUndefinedService
+		case rsp == nil:
+			// no need response
+			return nil, nil
+		default:
+			// send response
+			return rsp.ToBytes(), nil
+		}
+	}
+}
